fa6ddaeea3f842b54922fe400dd11face85ef835
Anuroop <anuroop.sirothia@gmail.com>
2021-05-26 01:59:02 +0530

    Slots info available
    Slots info like the address, availability is now getting fetched. Alert
    logic is pending.

--- src/main/java/anuroop/vaxalert/AppointmentAvailability.java
+++ src/main/java/anuroop/vaxalert/AppointmentAvailability.java
@@ -1,30 +1,15 @@
 package anuroop.vaxalert;
 
-import java.util.Arrays;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-
-import anuroop.vaxalert.domain.State;
-import anuroop.vaxalert.domain.StateList;
 
 @SpringBootApplication
 @EnableScheduling
 public class AppointmentAvailability {
-
-	private static final Logger log = LoggerFactory.getLogger(AppointmentAvailability.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppointmentAvailability.class, args);
@@ -33,25 +18,5 @@ public class AppointmentAvailability {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
-	}
-
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			// Setting HTTP Header is needed to avoid 403 Forbidden error.
-			HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-            HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-			
-            
-            ResponseEntity<StateList> stateListResponse = restTemplate.exchange("https://cdn-api.co-vin.in/api/v2/admin/location/states",HttpMethod.GET, entity, StateList.class);
-            StateList stateList = stateListResponse.getBody();
-
-			for(State state : stateList.getStates()) {
-				log.info(state.getStateName());
-			}
-			
-		};
 	}
 }
--- src/main/java/anuroop/vaxalert/SlotFinder.java
+++ src/main/java/anuroop/vaxalert/SlotFinder.java
@@ -15,8 +15,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import anuroop.vaxalert.domain.State;
-import anuroop.vaxalert.domain.StateList;
+import anuroop.vaxalert.domain.Session;
+import anuroop.vaxalert.domain.SessionList;
 
 /**
  * Checks for available slots periodically.
@@ -28,29 +28,42 @@ import anuroop.vaxalert.domain.StateList;
 public class SlotFinder {
 
 	private static final Logger log = LoggerFactory.getLogger(SlotFinder.class);
-	
+
 	@Autowired
 	private ApplicationContext context;
 
 	@Scheduled(fixedRate = 5000)
 	public void reportCurrentTime() {
-		
+
 
 		// Setting HTTP Header is needed to avoid 403 Forbidden error.
 		HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		
-        RestTemplate restTemplate = context.getBean(RestTemplate.class);
-        ResponseEntity<StateList> stateListResponse = restTemplate.exchange("https://cdn-api.co-vin.in/api/v2/admin/location/states",HttpMethod.GET, entity, StateList.class);
-        StateList stateList = stateListResponse.getBody();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
-		for(State state : stateList.getStates()) {
-			log.info(state.getStateName());
+		RestTemplate restTemplate = context.getBean(RestTemplate.class);
+		ResponseEntity<SessionList> sessionListtResponse = restTemplate.exchange("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=265&date=26-05-2021",HttpMethod.GET, entity, SessionList.class);
+		SessionList sessionList = sessionListtResponse.getBody();
+
+		for(Session session : sessionList.getSessions()) {
+
+			if(session.getMinAgeLimit() < 45) {
+				log.info("---------------");
+				log.info(session.getAddress());
+				log.info(session.getName());
+
+				log.info(session.getDate());
+
+				log.info(session.getAvailableCapacity().toString());
+				log.info(session.getAvailableCapacityDose1().toString());
+				log.info(session.getAvailableCapacityDose2().toString());
+
+				log.info(session.getMinAgeLimit().toString());
+				log.info(session.getSlots().toString());
+
+				log.info("\n");
+			}
 		}
-		
-	
-		
 	}
 }
